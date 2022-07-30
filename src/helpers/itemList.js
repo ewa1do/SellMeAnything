a9f9fb4a9afe27d99d58df55c101c57aecd55c69
ewa1do <this.eduardovera@gmail.com>
2022-07-29 22:57:11 -0400

    creating helper for the items list in the shopping cart

---
+++ src/helpers/itemList.js
@@ -0,0 +1,20 @@
+export const itemList = [
+  {
+    url: 'http://atrilco.com/wp-content/uploads/2017/11/ef3-placeholder-image.jpg',
+    desc: 'Random Item 1',
+    qty: 3,
+    price: 10,
+  },
+  {
+    url: 'http://atrilco.com/wp-content/uploads/2017/11/ef3-placeholder-image.jpg',
+    desc: 'Random Item 2',
+    qty: 5,
+    price: 20,
+  },
+  {
+    url: 'http://atrilco.com/wp-content/uploads/2017/11/ef3-placeholder-image.jpg',
+    desc: 'Random Item 3',
+    qty: 2,
+    price: 30,
+  },
+];
