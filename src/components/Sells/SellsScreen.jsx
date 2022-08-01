6ada656909357a549e780b81e9fa1e8c8076ea7f
ewa1do <this.eduardovera@gmail.com>
2022-08-01 17:49:06 -0400

    Creating Sells Component

--- src/components/Sells/SellsScreen.jsx
+++ src/components/Sells/SellsScreen.jsx
@@ -1,3 +1,23 @@
+import { LeftArrow } from '../UI/LeftArrow';
+import { ListComponent } from '../UI/ListComponent';
+import { SellsComponent } from './SellsComponent';
+
+import { orders } from '../../helpers/itemList.js';
+
 export const SellsScreen = () => {
-  return <div>SellsScreen</div>;
+  return (
+    <div>
+      <LeftArrow />
+
+      <div className='flex items-center justify-center text-2xl mb-20'>
+        <i className='pr-2'></i>
+        <h1>Sells</h1>
+      </div>
+
+      <ListComponent
+        list={orders}
+        Component={SellsComponent}
+      />
+    </div>
+  );
 };
