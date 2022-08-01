e6ace75ec360c4005f69f8f9022169c0366b5226
ewa1do <this.eduardovera@gmail.com>
2022-08-01 17:16:30 -0400

    Refactoring components to work with a reusable ListComponent

--- src/components/Cart/CartScreen.jsx
+++ src/components/Cart/CartScreen.jsx
@@ -1,16 +1,39 @@
 import { Link } from 'react-router-dom';
-import { IoCardOutline } from 'react-icons/io5';
+import {
+  IoCardOutline,
+  IoCheckboxOutline,
+} from 'react-icons/io5';
 
 import { ActionButtonWithIcon } from '../UI/ActionButtonWithIcon';
 import { LeftArrow } from '../UI/LeftArrow';
-import { CartItemList } from './CartItemList';
+import { ListComponent } from '../UI/ListComponent';
+import { CartItem } from './CartItem';
+import { itemList } from '../../helpers/itemList';
 
 export const CartScreen = () => {
   return (
     <div>
       <LeftArrow />
 
-      <CartItemList />
+      <div className='flex items-center justify-center text-2xl'>
+        <i className='pr-2'>
+          <IoCheckboxOutline />
+        </i>
+        <h1>Orders</h1>
+      </div>
+
+      <div className='mt-20'>
+        <ul className='flex justify-end px-8'>
+          <li className='px-16'>Item</li>
+          <li className='px-12'>Qty</li>
+          <li>Price</li>
+        </ul>
+
+        <ListComponent
+          list={itemList}
+          Component={CartItem}
+        />
+      </div>
 
       <Link
         to='/pay'
