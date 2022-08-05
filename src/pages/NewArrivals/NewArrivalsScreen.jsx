b50377710118b59c1db27c98735483e0e6739289
ewa1do <this.eduardovera@gmail.com>
2022-08-04 20:12:59 -0400

    creating the newArrivals screen to show items

--- src/pages/NewArrivals/NewArrivalsScreen.jsx
+++ src/pages/NewArrivals/NewArrivalsScreen.jsx
@@ -1,3 +1,18 @@
+import { ItemCard } from '../../components/cards/ItemCard';
+import { Navbar } from '../../components/Layout/Navbar/Navbar';
+import { ListComponent } from '../../components/lists/ListComponent';
+
+import { detailedItems } from '../../utilities/itemList.js';
+
 export const NewArrivalsScreen = () => {
-  return <div>New Arrivals</div>
-}
+  return (
+    <div className='p-3'>
+      <Navbar navigationArrow={false} />
+
+      <ListComponent
+        list={detailedItems}
+        Component={ItemCard}
+      />
+    </div>
+  );
+};
