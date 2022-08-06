44714ddd3b8618e64247ab3037de91728c89eb5b
ewa1do <this.eduardovera@gmail.com>
2022-08-05 21:24:16 -0400

    refactoring the LinkToHomePage component

--- src/components/navigation/LinkToHomePage.jsx
+++ src/components/navigation/LinkToHomePage.jsx
@@ -8,15 +8,16 @@ import { useNavigate } from 'react-router-dom';
 export const LinkToHomePage = () => {
   // TODO: Create a logo for the component
   const navigate = useNavigate();
+
   const goToHomePage = () => navigate('/', { replace: true });
 
   return (
-    <div className='my-3 mx-3 flex'>
+    <div className='my-3 mx-2 flex'>
       <span
-        className='text-md cursor-pointer'
+        className='text-md cursor-pointer italic border-2 border-slate-900 p-1 rounded-lg'
         onClick={goToHomePage}
       >
-        LOGO
+        SMA
       </span>
     </div>
   );
