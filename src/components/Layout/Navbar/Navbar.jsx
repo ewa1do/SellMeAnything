14bad4559b8a03b406095aa669a4df2ed51a0426
ewa1do <this.eduardovera@gmail.com>
2022-08-05 21:21:49 -0400

    refactoring navbar with a conditional rendering

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,14 +1,17 @@
 import { IoPersonOutline, IoBagOutline } from 'react-icons/io5';
 
 import { SearchInput } from '../../inputs/SearchInput';
+import { LeftArrow } from '../../navigation/LeftArrow';
+import { LinkToHomePage } from '../../navigation/LinkToHomePage';
 import { NavbarItem } from './NavbarItem';
 
-export const Navbar = () => {
+export const Navbar = ({ navigationArrow }) => {
   return (
-    <div className='flex flex-row justify-between'>
+    <div className='flex flex-row items-center'>
+      {navigationArrow ? <LeftArrow /> : <LinkToHomePage />}
       <SearchInput />
 
-      <div className='flex flex-row'>
+      <div className='flex flex-row ml-24'>
         <NavbarItem
           path='profile'
           icon={<IoPersonOutline />}
