140a41344f94106e1fc0d9008e827feda87a12ed
ewa1do <this.eduardovera@gmail.com>
2022-08-06 23:13:17 -0400

    refatoring fetchData

--- src/utilities/fetchData.js
+++ src/utilities/fetchData.js
@@ -1,9 +1,17 @@
+import { min } from 'moment';
 import { API_URL_TEST } from './config';
 
-export const fetchData = async (endpoint, limit) => {
-  const baseURL = `${API_URL_TEST}/${endpoint}/${
-    limit ? '?limit=' + limit : ''
+// export const fetchData = async (endpoint, limit, skip) => {
+export const fetchData = async (endpoint, options) => {
+  let baseURL = `${API_URL_TEST}/${endpoint}/${
+    options.limit ? '?limit=' + options.limit : ''
   }`;
+
+  if (options.skip) {
+    baseURL += baseURL.includes('?')
+      ? '&skip=' + options.skip
+      : '?skip=' + options.skip;
+  }
 
   const res = await fetch(baseURL);
 
