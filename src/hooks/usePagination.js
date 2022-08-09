d12a808d0f4568c9bc453ad5a68baa5b427dcd2e
ewa1do <this.eduardovera@gmail.com>
2022-08-09 19:26:48 -0400

    Refactoring hook usePagination

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,81 +1,28 @@
-import { useMemo } from 'react';
-import { range } from '../utilities/range';
+import { useState } from 'react';
+import { useFetch } from './useFetch';
 
-/**
- * @param {*} totalCount the total data available from the source
- * @param {*} pageSize maximum data visible in a single page
- * @param {*} siblingCount min number of page buttons to be shown on each side of the current page button
- * @param {*} currentPage current active page
- * @returns Range of numbers to be displayed as an array
- */
+export const usePagination = (category) => {
+  const { data, loading } = useFetch(category);
 
-export const usePagination = (
-  totalCount,
-  pageSize,
-  siblingCount = 1,
-  currentPage
-) => {
-  const paginationRange = useMemo(() => {
-    const totalPageCount = Math.ceil(totalCount / pageSize);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(5);
 
-    const totalPageNumbers = siblingCount + 5;
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
-    // CASE 1: number of pages < pages numbers we want to show return the [1...totalPageCount]
-    if (totalPageNumbers >= totalPageCount) {
-      return range(1, totalPageCount);
-    }
+  const currentItems = data.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
 
-    const leftSiblingIndex = Math.max(
-      currentPage - siblingCount,
-      1
-    );
-    const rightSiblingindex = Math.min(
-      currentPage + siblingCount,
-      totalPageCount
-    );
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
-    const shouldShowLeftDots = leftSiblingIndex > 2;
-    const shouldShowRightDots =
-      rightSiblingindex < totalPageCount - 2;
-
-    const firstPageIndex = 1;
-    const lastPageIndex = totalPageCount;
-
-    // CASE 2: Only rights dots to be show
-    if (!shouldShowLeftDots && shouldShowRightDots) {
-      let leftItemCount = 3 + 2 * siblingCount;
-      let leftRange = range(1, leftItemCount);
-
-      return [...leftRange, DOTS, totalPageCount];
-    }
-
-    // CASE 3: Only left dots to be shown
-    if (shouldShowLeftDots && !shouldShowRightDots) {
-      let rightItemCount = 3 + 2 * siblingCount;
-      let rightRange = range(
-        totalPageCount - rightItemCount + 1,
-        totalPageCount
-      );
-
-      return [firstPageIndex, DOTS, ...rightRange];
-    }
-
-    // CASE 4: Both left and right dots to be shown
-    if (shouldShowLeftDots && shouldShowRightDots) {
-      let middleRange = range(
-        leftSiblingIndex,
-        rightSiblingindex
-      );
-
-      return [
-        firstPageIndex,
-        DOTS,
-        ...middleRange,
-        DOTS,
-        lastPageIndex,
-      ];
-    }
-  }, [totalCount, pageSize, siblingCount, currentPage]);
-
-  return paginationRange;
+  return {
+    data,
+    loading,
+    currentItems,
+    itemsPerPage,
+    currentPage,
+    paginate,
+  };
 };
