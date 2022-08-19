0df17c9e60b1059a934e26b78b187bf6c740fa9b
ewa1do <this.eduardovera@gmail.com>
2022-08-19 11:51:20 -0400

    adding the total price feature on the reducer

--- src/redux/states/cartReducer.js
+++ src/redux/states/cartReducer.js
@@ -2,15 +2,20 @@ import { types } from '../types/types';
 
 const initialState = {
   items: [],
-  // total: null,
 };
 
 export const cartReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.cartAddNewItem:
+      const totalPrice =
+        action.payload.price * action.payload.qty;
+
       return {
         ...state,
-        items: [...state.items, action.payload],
+        items: [
+          ...state.items,
+          { ...action.payload, totalPrice },
+        ],
       };
     default:
       return state;
