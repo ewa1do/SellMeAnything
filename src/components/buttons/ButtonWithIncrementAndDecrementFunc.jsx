71902f739ef575c3c4c9d2d6775845386ee87a11
ewa1do <this.eduardovera@gmail.com>
2022-08-20 14:58:30 -0400

    create a button to handle the products quantity

---
+++ src/components/buttons/ButtonWithIncrementAndDecrementFunc.jsx
@@ -0,0 +1,31 @@
+import { useQuantity } from '../../hooks/useQuantity';
+
+export const ButtonWithIncrementAndDecrementFunc = ({
+  initialValue,
+  limit,
+}) => {
+  const { productQty, incrementQty, decrementQty } = useQuantity(
+    initialValue,
+    limit
+  );
+
+  return (
+    <div className='flex items-center mr-2 mt-2 border-2 border-gray-600 rounded-md h-12'>
+      <button
+        className='text-xl px-2 text-slate-800'
+        onClick={increment}
+      >
+        -
+      </button>
+      <span className='text-xl text-gray-800'>
+        {initialValue}
+      </span>
+      <button
+        className='text-xl px-2 text-slate-800'
+        onClick={decrement}
+      >
+        +
+      </button>
+    </div>
+  );
+};
