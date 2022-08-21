def073d1866bcec797d583a3704e12574d3bdcdd
ewa1do <this.eduardovera@gmail.com>
2022-08-21 15:31:47 -0400

    Refactoring cartScreen to render the data in the store

--- src/pages/Cart/CartScreen.jsx
+++ src/pages/Cart/CartScreen.jsx
@@ -1,3 +1,5 @@
+import { useSelector } from 'react-redux';
+import { v4 as uuid } from 'uuid';
 import { Link } from 'react-router-dom';
 import {
   IoCardOutline,
@@ -6,45 +8,55 @@ import {
 
 import { ActionButtonWithIcon } from '../../components/buttons/ActionButtonWithIcon';
 import { LeftArrow } from '../../components/navigation/LeftArrow';
-import { ListComponent } from '../../components/lists/ListComponent';
 import { CartItem } from './CartItem';
 
-import { itemList } from '../../helpers/itemList';
-
 export const CartScreen = () => {
+  const { items: cartItems } = useSelector(
+    (state) => state.cart
+  );
+
   return (
     <div>
       <LeftArrow />
 
-      <div className='flex items-center justify-center text-2xl'>
-        <i className='pr-2'>
-          <IoCheckboxOutline />
-        </i>
-        <h1>Orders</h1>
-      </div>
+      {cartItems.length !== 0 ? (
+        <>
+          <div className='flex items-center justify-center text-2xl'>
+            <i className='pr-2'>
+              <IoCheckboxOutline />
+            </i>
+            <h1>Orders</h1>
+          </div>
 
-      <div className='mt-20'>
-        <ul className='flex justify-end px-8'>
-          <li className='px-16'>Item</li>
-          <li className='px-12'>Qty</li>
-          <li>Price</li>
-        </ul>
+          <div className='flex justify-center mt-20'>
+            <ul className='flex flex-col justify-end px-8 pb-8 w-3/4'>
+              {cartItems.map((item, i) => {
+                return (
+                  <CartItem
+                    key={uuid()}
+                    props={{
+                      ...item,
+                      iteration: i,
+                    }}
+                  />
+                );
+              })}
+            </ul>
+          </div>
 
-        <ListComponent
-          list={itemList}
-          Component={CartItem}
-        />
-      </div>
-
-      <Link
-        to='/pay'
-        className='flex justify-center mt-40'
-      >
-        <ActionButtonWithIcon
-          icon={<IoCardOutline />}
-          value='Pay'
-        />
-      </Link>
+          <Link
+            to='/pay'
+            className='flex justify-center mt-40'
+          >
+            <ActionButtonWithIcon
+              Icon={IoCardOutline}
+              value='Pay'
+            />
+          </Link>
+        </>
+      ) : (
+        <div>No data</div>
+      )}
     </div>
   );
 };
