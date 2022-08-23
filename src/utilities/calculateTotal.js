c24483084da04a8c913347b829f038fc1cd1e21b
ewa1do <this.eduardovera@gmail.com>
2022-08-22 20:40:43 -0400

    refactoring calculateTotal util

--- src/utilities/calculateTotal.js
+++ src/utilities/calculateTotal.js
@@ -1,13 +1,20 @@
+/**
+ *
+ * @param {array} arr Array of objects to map the price and the quantity
+ * @returns An array containing the subtotal, the shoppingCost and the total of the cart
+ */
+
 export const calculateSubtotalShippingAndTotal = (arr = []) => {
   let subtotal = 0;
+
   arr.forEach((item) => {
     subtotal += item.price * item.qty;
   });
+
   const shippingCost = subtotal * 0.03;
   const total = subtotal + shippingCost;
-  return [
-    subtotal.toFixed(2),
-    shippingCost.toFixed(2),
-    total.toFixed(2),
-  ];
+
+  return [subtotal, shippingCost, total].map((price) =>
+    price.toFixed(2)
+  );
 };
