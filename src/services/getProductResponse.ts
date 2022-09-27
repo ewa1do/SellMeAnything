fa421452ed96381bcaed080895082bd3eef49477
ewa1do <this.eduardovera@gmail.com>
2022-09-27 14:07:51 -0400

    :alien: refactor on code due to api changes

--- src/services/getProductResponse.ts
+++ src/services/getProductResponse.ts
@@ -4,10 +4,12 @@ const url = import.meta.env.VITE_APP_API_BASE_URL;
 
 export async function getProductResponse(): Promise<ProductResponse[]> {
   const response = await fetch(`${url}/product`);
+
   try {
     const { data } = await response.json();
 
-    const { products }: { products: ProductResponse[] } = data.body;
+    const { body: products }: { body: ProductResponse[] } = data;
+
     return products;
   } catch (error) {
     throw new Error('Error accessing products');
