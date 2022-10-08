c31c071f3bb8203a9e91bd91d49960fea6e9d4b4
ewa1do <this.eduardovera@gmail.com>
2022-10-08 13:26:16 -0400

    :recycle: refactoring typos and interfaces in the product page

--- src/pages/Products/Product.tsx
+++ src/pages/Products/Product.tsx
@@ -1,7 +1,31 @@
+import { createContext, useCallback, useEffect, useState } from 'react';
+import { ProductResponse } from '@/models';
+
 interface Props {
   children: React.ReactElement | React.ReactElement[];
+  product: ProductResponse;
 }
 
-export const Product = ({ children }: Props) => {
-  return <div className='flex flex-col mt-6 p-2 w-4/6 shadow-leever-shadow rounded-lg'>{children}</div>;
+export interface ContextProduct extends ProductResponse {
+  qty?: number;
+}
+
+export const ProductContext = createContext({} as ContextProduct);
+
+const { Provider } = ProductContext;
+
+export const Product = ({ children, product }: Props) => {
+  const [productState, setProductState] = useState(product);
+
+  const setPageContext = useCallback(() => {
+    setProductState({ ...productState });
+  }, [productState]);
+
+  useEffect(() => setPageContext(), []);
+
+  return (
+    <Provider value={productState}>
+      <div className='flex flex-col mt-6 p-2 w-4/6 shadow-leever-shadow rounded-lg'>{children}</div>
+    </Provider>
+  );
 };
